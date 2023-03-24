342474af655038e8439fafaa4953c0133d957a8a
Bouchra Akl <bushraakl1234@gmail.com>
2023-03-24 14:04:43 -0300

    Class Implementation Corrections

---
+++ src/main/java/br/com/uniamerica/gpc/GPCbackend/entity/Responsavel.java
@@ -0,0 +1,14 @@
+//------------------Package----------------------
+package br.com.uniamerica.gpc.GPCbackend.entity;
+
+//------------------Imports----------------------
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+//------------------------------------------------
+@Entity
+@Table(name = "responsavel",schema = "public")
+public class Responsavel extends AbstractClass {
+
+}
