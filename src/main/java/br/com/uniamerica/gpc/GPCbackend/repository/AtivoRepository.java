f44bde0c267825edb3c4d85758b599ce2a2fc8a1
Jean Moschen - Home <jeanfelipe0610@gmail.com>
2023-04-19 20:12:56 -0300

    melhorias de repository 'Ativo' e 'Beneficiario'

--- src/main/java/br/com/uniamerica/gpc/GPCbackend/repository/AtivoRepository.java
+++ src/main/java/br/com/uniamerica/gpc/GPCbackend/repository/AtivoRepository.java
@@ -1,6 +1,7 @@
 package br.com.uniamerica.gpc.GPCbackend.repository;
 
 import br.com.uniamerica.gpc.GPCbackend.entity.Ativo;
+import br.com.uniamerica.gpc.GPCbackend.entity.Categoria;
 import br.com.uniamerica.gpc.GPCbackend.entity.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,11 +12,11 @@ import java.util.List;
 
 @Repository
 public interface AtivoRepository extends JpaRepository <Ativo, Long> {
-    @Query("from Ativo where cod_patrimonio like :idPatrimonio")
+    @Query("from Ativo where idPatrimonio like :idPatrimonio")
     public List<Ativo> findByAtivo(@Param("idPatrimonio") final String idPatrimonio);
 
-
-
-
+    /* Query para buscar o ativo atraves do nome da categoria*/
+    @Query("select ativo from Ativo ativo join ativo.categoria categoria where categoria.nomeCategoria = :nomeCategoria")
+    public List<Ativo> findByCategoria(@Param("nomeCategoria") String nomeCategoria);
 
 }
--- src/main/java/br/com/uniamerica/gpc/GPCbackend/repository/BeneficiarioRepository.java
+++ src/main/java/br/com/uniamerica/gpc/GPCbackend/repository/BeneficiarioRepository.java
@@ -1,9 +1,16 @@
 package br.com.uniamerica.gpc.GPCbackend.repository;
 
+import br.com.uniamerica.gpc.GPCbackend.entity.Ativo;
 import br.com.uniamerica.gpc.GPCbackend.entity.Beneficiario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BeneficiarioRepository extends JpaRepository <Beneficiario, Long> {
+    @Query("select beneficiario from Beneficiario beneficiario join beneficiario.perfil perfil where perfil.nome = :nome")
+    public List<Beneficiario> findByNome(@Param("nome") String nome);
 }
