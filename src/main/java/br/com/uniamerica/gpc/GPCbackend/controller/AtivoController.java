3889e4caa698158d74ff8d97664675191bf17112
Bouchra Akl <bushraakl1234@gmail.com>
2023-05-02 20:03:10 -0300

    Criando a Estrutura do Controller

---
+++ src/main/java/br/com/uniamerica/gpc/GPCbackend/controller/AtivoController.java
@@ -0,0 +1,12 @@
+//------------------Package----------------------
+package br.com.uniamerica.gpc.GPCbackend.controller;
+
+//------------------Imports----------------------
+
+import jakarta.persistence.Entity;
+
+//------------------------------------------------
+
+public class AtivoController {
+
+}
---
+++ src/main/java/br/com/uniamerica/gpc/GPCbackend/controller/MovimentacaoController.java
@@ -0,0 +1,10 @@
+//------------------Package----------------------
+package br.com.uniamerica.gpc.GPCbackend.controller;
+
+//------------------Imports----------------------
+
+
+
+//------------------------------------------------
+public class MovimentacaoController {
+}
