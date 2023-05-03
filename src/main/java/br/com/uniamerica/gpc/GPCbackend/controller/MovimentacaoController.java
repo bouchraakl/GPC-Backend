b192bf32ffcbc1bbe587e5eb2650845ffef6d79a
Jean Moschen - Home <jeanfelipe0610@gmail.com>
2023-05-03 20:28:43 -0300

    Movimentacao.java - Repository com Query's criadas e Controller inicializado

--- src/main/java/br/com/uniamerica/gpc/GPCbackend/controller/MovimentacaoController.java
+++ src/main/java/br/com/uniamerica/gpc/GPCbackend/controller/MovimentacaoController.java
@@ -4,7 +4,17 @@ package br.com.uniamerica.gpc.GPCbackend.controller;
 //------------------Imports----------------------
 
 
+import br.com.uniamerica.gpc.GPCbackend.repository.MovimentacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 //------------------------------------------------
+@Controller
+@RequestMapping("/movimentacoes")
 public class MovimentacaoController {
+    @Autowired
+    private MovimentacaoRepository movimentacaoRepository;
+
 }
+
--- src/main/java/br/com/uniamerica/gpc/GPCbackend/repository/MovimentacaoRepository.java
+++ src/main/java/br/com/uniamerica/gpc/GPCbackend/repository/MovimentacaoRepository.java
@@ -1,6 +1,5 @@
 package br.com.uniamerica.gpc.GPCbackend.repository;
 
-import br.com.uniamerica.gpc.GPCbackend.entity.Ativo;
 import br.com.uniamerica.gpc.GPCbackend.entity.Movimentacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,17 +9,43 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * @author Jean Moschen
+ * */
+
 @Repository
 public interface MovimentacaoRepository extends JpaRepository <Movimentacao, Long> {
-    @Query("from Movimentacao where Movimentacao.beneficiario.perfil.nome like :nome")
-    public List<Movimentacao> findByMovimentacao(@Param("nome") String nome);
+    /**
+     * @param beneficiarioId ID do Beneficiario {@link br.com.uniamerica.gpc.GPCbackend.entity.Beneficiario}, para filtrar as Movimentações {@link Movimentacao}
+     * @return Lista de movimentações que contêm o beneficiário informado
+     */
+    @Query("from Movimentacao where beneficiario.id = :beneficiarioId")
+    public List<Movimentacao> findByBeneficiarioId(@Param("beneficiarioId") Long beneficiarioId);
 
-    @Query("from Movimentacao where Movimentacao.ativo.categoria.nomeCategoria like :nomeCategoria")
-    public List<Movimentacao> findByAtivoCategoria(@Param("nomeCategoria") String nomeCategoria);
+    /**
+     * @param categoriaId ID da Categoria {@link br.com.uniamerica.gpc.GPCbackend.entity.Categoria}, para filtrar as Movimentações {@link Movimentacao}
+     * @return Lista de Movimentações que contêm a categoria informada
+     */
+    @Query("from Movimentacao where ativo.categoria.id = :categoriaId")
+    public List<Movimentacao> findByAtivoCategoriaId(@Param("categoriaId") Long categoriaId);
 
-    @Query("from Movimentacao where Movimentacao.dataEmprestimo = :dataEmprestimo")
+    /**
+     * @param dataEmprestimo Data de Incio do empréstimo, para filtrar as Movimentações
+     * @return Lista de Movimentações que foram iniciadas na data informada
+     */
+    @Query("from Movimentacao where dataEmprestimo = :dataEmprestimo")
     public List<Movimentacao> findByDataEmprestimo(@Param("dataEmprestimo") LocalDate dataEmprestimo);
-    @Query("from Movimentacao where Movimentacao.dataDevolucao = :dataEmprestimo")
+
+    /**
+     * @param dataDevolucao Data de Devolução do empréstimo, para filtrar as Movimentações
+     * @return Lista de Movimentações que foram encerradas na data informada ou estão previstas para serem encerradas.
+     */
+    @Query("from Movimentacao where dataDevolucao = :dataEmprestimo")
     public List<Movimentacao> findByDataDevolucao(@Param("dataDevolucao") LocalDate dataDevolucao);
 
+    /**
+     * @return Todas as movimentações em que o Ativo ainda não foi devolido, portanto, todas as movimentações em aberto
+     */
+    @Query("from Movimentacao  where isDevolvido = true")
+    public List<Movimentacao> findAllAbertas();
 }
