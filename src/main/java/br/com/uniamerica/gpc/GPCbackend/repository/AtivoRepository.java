bca276e5966a7cbb20484353d9704568110465f4
Bouchra Akl <bushraakl1234@gmail.com>
2023-05-04 15:31:25 -0300

    AtivoRepository.java - Implementação de consultas (@query) finalizadas

--- src/main/java/br/com/uniamerica/gpc/GPCbackend/repository/AtivoRepository.java
+++ src/main/java/br/com/uniamerica/gpc/GPCbackend/repository/AtivoRepository.java
@@ -1,8 +1,9 @@
+//------------------Package----------------------
 package br.com.uniamerica.gpc.GPCbackend.repository;
 
-import br.com.uniamerica.gpc.GPCbackend.entity.Ativo;
-import br.com.uniamerica.gpc.GPCbackend.entity.Categoria;
-import br.com.uniamerica.gpc.GPCbackend.entity.Pessoa;
+//------------------Imports----------------------
+
+import br.com.uniamerica.gpc.GPCbackend.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,11 +11,44 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-public interface AtivoRepository extends JpaRepository <Ativo, Long> {
-    @Query("from Ativo where idPatrimonio like :idPatrimonio")
-    public List<Ativo> findByAtivo(@Param("idPatrimonio") String idPatrimonio);
+//------------------------------------------------
 
-    @Query("from Ativo ativo join ativo.categoria categoria where categoria.nomeCategoria = :nomeCategoria")
-    public List<Ativo> findByCategoria(@Param("nomeCategoria") String nomeCategoria);
+/**
+ * @author Bouchra Akl
+ */
+
+@Repository
+public interface AtivoRepository extends JpaRepository<Ativo, Long> {
+
+    /**
+     * @param condicao a ser utilizada como critério de busca
+     * @return uma lista de objetos Ativo que satisfazem à condição informada
+     */
+    @Query("SELECT a FROM Ativo a WHERE a.condicao = :condicao")
+    List<Ativo> findByCondicao(@Param("condicao") Condicao condicao);
+
+    /**
+     * @param status o status a ser utilizado como critério de busca
+     * @return uma lista de objetos Ativo que possuem o status informado
+     */
+    @Query("SELECT a FROM Ativo a WHERE a.status = :status")
+    public List<Ativo> findByStatus(@Param("status") final Status status);
+
+    /**
+     * @param idPatrimonio o idPatrimonio do objeto Ativo a ser retornado
+     * @return o objeto Ativo com o idPatrimonio especificado
+     */
+    public Ativo findByIdPatrimonio(String idPatrimonio);
+
+    /**
+     * Retorna uma lista de objetos {@link Ativo} cujo objeto {@link Categoria}
+     * associado tem um campo "nomeCategoria" igual ao valor fornecido.
+     *
+     * @param nomeCategoria o valor a ser comparado com o campo "nomeCategoria"
+     * @return uma lista de objetos {@link Ativo} correspondentes
+     */
+    @Query("SELECT a FROM Ativo a WHERE a.categoria.nomeCategoria = :nomeCategoria")
+    public List<Ativo> findByNomeCategoria(@Param("nomeCategoria") final String nomeCategoria);
+
+
 }
