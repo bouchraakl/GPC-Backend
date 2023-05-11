d04d2e4577955562a33a0747990adbbc00bf4b97
Gustavo Piegat <gustavopiegat2004@gmail.com>
2023-05-11 19:47:36 -0300

    EnderecoController -- Metodos Get prontos

--- src/main/java/br/com/uniamerica/gpc/GPCbackend/controller/EnderecoController.java
+++ src/main/java/br/com/uniamerica/gpc/GPCbackend/controller/EnderecoController.java
@@ -4,11 +4,76 @@ package br.com.uniamerica.gpc.GPCbackend.controller;
 //------------------Imports----------------------
 
 
+import br.com.uniamerica.gpc.GPCbackend.entity.Endereco;
+import br.com.uniamerica.gpc.GPCbackend.repository.EnderecoRepository;
+import br.com.uniamerica.gpc.GPCbackend.service.EnderecoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //------------------------------------------------
 @Controller
 @RequestMapping("/enderecos")
 public class EnderecoController {
+
+
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+    @Autowired
+    private EnderecoService enderecoService;
+
+
+
+    @GetMapping
+    public ResponseEntity<?> getByCep(@RequestParam("cep")String cep){
+
+        final Endereco endereco = this.enderecoRepository.findByCep(cep);
+        return endereco == null ? ResponseEntity.badRequest().body("nenhum endereço encontrado") : ResponseEntity.ok(endereco);
+
+    }
+
+    @GetMapping("/logradouro")
+    public ResponseEntity<?> getByLogradouro(@RequestParam("logradouro")String logradouro){
+
+        final Endereco endereco = this.enderecoRepository.findByLogradouro(logradouro);
+        return endereco == null ? ResponseEntity.badRequest().body("nenhum endereço encontrado") : ResponseEntity.ok(endereco);
+
+    }
+
+    @GetMapping("/bairro")
+    public ResponseEntity<?> getByBairro(@RequestParam("bairro")String bairro){
+
+        final Endereco endereco = this.enderecoRepository.findByBairro(bairro);
+        return endereco == null ? ResponseEntity.badRequest().body("nenhum endereço encontrado") : ResponseEntity.ok(endereco);
+
+    }
+
+    @GetMapping("/cidade")
+    public ResponseEntity<?> getByCidade(@RequestParam("cidade")String cidade){
+
+        final Endereco endereco = this.enderecoRepository.findByCidade(cidade);
+        return endereco == null ? ResponseEntity.badRequest().body("nenhum endereço encontrado") : ResponseEntity.ok(endereco);
+
+    }
+
+    @GetMapping("/uf")
+    public ResponseEntity<?> getByUf(@RequestParam("uf")String uf){
+
+        final Endereco endereco = this.enderecoRepository.findByUf(uf);
+        return endereco == null ? ResponseEntity.badRequest().body("nenhum endereço encontrado") : ResponseEntity.ok(endereco);
+
+    }
+
+    @GetMapping("/uf")
+    public ResponseEntity<?> getByPais(@RequestParam("pais")String pais){
+
+        final Endereco endereco = this.enderecoRepository.findByPais(pais);
+        return endereco == null ? ResponseEntity.badRequest().body("nenhum endereço encontrado") : ResponseEntity.ok(endereco);
+
+    }
+
 }
--- src/main/java/br/com/uniamerica/gpc/GPCbackend/repository/EnderecoRepository.java
+++ src/main/java/br/com/uniamerica/gpc/GPCbackend/repository/EnderecoRepository.java
@@ -13,26 +13,26 @@ import java.util.List;
 @Repository
 public interface EnderecoRepository extends JpaRepository <Endereco, Long> {
 
-    @Query("from Endereco where cep = :cep")
-    public List<Endereco> findByCep(@Param("cep") String cep);
+    @Query("from Endereco where cep like :cep")
+    public Endereco findByCep(@Param("cep") String cep);
 
     @Query("from Endereco where Endereco = :Endereco")
-    public List<Endereco> findByAll(@Param("lista") String Endereco);
+    public Endereco findByAll(@Param("lista") String Endereco);
 
     @Query("from Endereco where logradouro = :logradouro")
-    public List<Endereco> findByLogradouro(@Param("logradouro") String logradouro);
+    public Endereco findByLogradouro(@Param("logradouro") String logradouro);
 
     @Query("from Endereco where bairro = :bairro")
-    public List<Endereco> findByBairro(@Param("bairro") String Bairro);
+    public Endereco findByBairro(@Param("bairro") String Bairro);
 
     @Query("from Endereco where cidade = :cidade")
-    public List<Endereco> findByCidade(@Param("cidade") String Cidade);
+    public Endereco findByCidade(@Param("cidade") String Cidade);
 
     @Query("from Endereco where uf = :uf")
-    public List<Endereco> findByUf(@Param("uf") String uf);
+    public Endereco findByUf(@Param("uf") String uf);
 
     @Query("from Endereco where pais = :pais")
-    public List<Endereco> findByPais(@Param("pais") String pais);
+    public Endereco findByPais(@Param("pais") String pais);
 
 
 }
