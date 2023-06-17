1a89972fc39fb30578f37121e41a7e7226044656
Bouchra Akl <bushraakl1234@gmail.com>
2023-06-17 12:14:11 -0300

    Merge.

--- src/main/java/br/com/uniamerica/gpc/GPCbackend/entity/Endereco.java
+++ src/main/java/br/com/uniamerica/gpc/GPCbackend/entity/Endereco.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.envers.AuditTable;
@@ -22,31 +23,37 @@ public class Endereco extends AbstractEntity {
 
     @Getter
     @Setter
+    @Size(min = 3, max = 50, message = "Tamanho invalido! MIN(3) MAX(50)")
     @Column( nullable = false, length = 50)
     private String logradouro;
 
     @Getter
     @Setter
+    @Size(min = 3, max = 20, message = "Tamanho invalido! MIN(3) MAX(20)")
     @Column(nullable = false, length = 20)
     private String bairro;
 
     @Getter
     @Setter
+    @Size(min = 3, max = 20, message = "Tamanho invalido! MIN(3) MAX(20)")
     @Column(nullable = false, length = 20)
     private String cidade;
 
     @Getter
     @Setter
+    @Size(min = 2, max = 3, message = "Tamanho invalido! MIN(1) MAX(3)")
     @Column(nullable = false, length = 3)
     private String uf;
 
     @Getter
     @Setter
+    @Size(min = 3, max = 15, message = "Tamanho invalido! MIN(3) MAX(15)")
     @Column(nullable = false, length = 15)
     private String pais;
 
     @Getter
     @Setter
+    @Size(min = 3, max = 15, message = "Tamanho invalido! MIN(3) MAX(15)")
     @Column(nullable = false, length = 15)
     private String cep;
 
